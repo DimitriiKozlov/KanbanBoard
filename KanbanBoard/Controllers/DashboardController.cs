a8bc6f1d78d100ec3faf7194344d2b89a5653a2b
Dimitrii Kozlov <ikzdimitrij@gmail.com>
2018-02-05 15:46:19 +0200

    Add BusinessLogic and update DbContext

--- KanbanBoard/Controllers/DashboardController.cs
+++ KanbanBoard/Controllers/DashboardController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using KanbanBoard.Models;
 
 namespace KanbanBoard.Controllers
@@ -11,6 +8,10 @@ namespace KanbanBoard.Controllers
     //[Route("api/[controller]")]
     public class DashboardController : Controller
     {
+        private BusinessLogic _businessLogic;
+
+        public BusinessLogic BusinessLogic => _businessLogic ?? (_businessLogic = new BusinessLogic());
+
         //GET: Dashboard
         public IActionResult Index()
         {
@@ -20,95 +21,42 @@ namespace KanbanBoard.Controllers
         [HttpGet]
         public IEnumerable<Card> GetAll()
         {
-            using (var context = new DashboardContext(new DbContextOptions<DashboardContext>()))
-            {
-                var dashboardContext = context.Cards.Include(c => c.State);
-                return dashboardContext.ToList();
-            }
+            return BusinessLogic.GetAllCards();
         }
 
         // GET: Dashboard/Create
         public IActionResult Create()
         {
-            using (var context = new DashboardContext(new DbContextOptions<DashboardContext>()))
-            {
-                ViewData["StatePriority"] = new SelectList(context.States, "Priority", "Name");
-            }
             return View();
         }
 
         [HttpPost]
         public IActionResult PostCard([FromBody]Card card)
         {
-            using (var context = new DashboardContext(new DbContextOptions<DashboardContext>()))
-            {
-                card.Id = Guid.NewGuid();
-                card.State = context.States.FirstOrDefault(s => s.Name == "ToDo");
-                context.Add(card);
-                context.SaveChanges();
-            }
+            BusinessLogic.AddCard(card);
             return Ok(card);
         }
 
         [HttpPost]
         public IActionResult UpdateCard([FromBody]Card uCard)
         {
-            using (var context = new DashboardContext(new DbContextOptions<DashboardContext>()))
-            {
-                var card = context.Cards.FirstOrDefault(c => c.Id == uCard.Id);
-                if (card == null)
-                    return NotFound();
-                try
-                {
-                    card.Title = uCard.Title;
-                    card.Description = uCard.Description;
-                    context.SaveChanges();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    return NotFound();
-                }
-
-                var dashboardContext = context.Cards.Include(c => c.State);
-                return Ok(dashboardContext.FirstOrDefault(c => c.Id == uCard.Id));
-            }
+            var card = BusinessLogic.UpdateCard(uCard);
+            return card == null ? (IActionResult)NotFound() : Ok(card);
         }
 
         [HttpGet]
         public IActionResult ChangeStatusCard(Guid id)
         {
-            using (var context = new DashboardContext(new DbContextOptions<DashboardContext>()))
-            {
-                var card = context.Cards.Include(c => c.State).SingleOrDefault(m => m.Id == id);
-                if (card == null)
-                {
-                    return NotFound();
-                }
-
-                card.State = context.States.FirstOrDefault(s => s.Priority == card.State.Priority + 1);
-                context.SaveChanges();
-
-                var dashboardContext = context.Cards.Include(c => c.State);
-                return Ok(dashboardContext.FirstOrDefault(c => c.Id == card.Id));
-            }
+            var card = BusinessLogic.ChangeStatusCard(id);
+            return card == null ? (IActionResult)NotFound() : Ok(card);
         }
 
 
         [HttpDelete]
-        public IActionResult DeleteCard(string id)
+        public IActionResult DeleteCard(Guid id)
         {
-            using (var context = new DashboardContext(new DbContextOptions<DashboardContext>()))
-            {
-                var card = context.Cards.SingleOrDefault(m => m.Id == new Guid(id));
-                if (card == null)
-                {
-                    return NotFound();
-                }
-
-                context.Cards.Remove(card);
-                context.SaveChanges();
-                return Ok(card);
-            }
+            var card = BusinessLogic.DeleteCard(id);
+            return card == null ? (IActionResult)NotFound() : Ok(card);
         }
     }
 }
